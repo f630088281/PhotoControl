c764bb3a537658bb3ce46721930bc90d2e21c130
FuJH <630088281@qq.com>
2016-05-26 12:57:19 +0800

    dev
    xx

--- app/src/main/java/im/youyu/photocontrol/NoDialogFrag.java
+++ app/src/main/java/im/youyu/photocontrol/NoDialogFrag.java
@@ -86,6 +86,8 @@ public class NoDialogFrag extends Fragment implements View.OnClickListener{
                     break;
             }
     //aaaaaaaa
+
+            //aaaaxxx
         }
 
         @Override
